d3b82ca8831e7c39a12ab69800daa9447284d400
Duane Bekaert <duane.bekaert@gmail.com>
2014-12-16 18:20:11 +0100

    The long lines are broken into smaller sub-string according the wraping length.

--- src/main.cpp
+++ src/main.cpp
@@ -2,22 +2,38 @@
 #include <string>
 #include <exception>
 #include <stdexcept>
+#include <functional>
+#include <algorithm> 
+#include <cctype>
+#include <locale>
+#include <vector>
+#include <map>
 
 extern "C" {
     #include <unistd.h>
     #include <getopt.h>
 }
 
+typedef std::map<std::string, std::string> rep_map;
+typedef rep_map::iterator it_rep_map;
+
 /* functions */
 void display_usage(std::string exname, std::string e_version);
-
 void display_cow_list(std::string e_cowpath);
-
 void display_cow();
 
 void add_word_to_message(const std::string & word);
 
 void get_interactive_message();
+
+std::vector<std::string> break_down_message();
+
+std::string multi_replace(std::string & str, rep_map e_rep_map);
+
+/* simple and efficient trim funcions, from : http://stackoverflow.com/questions/216823/whats-the-best-way-to-trim-stdstring */
+static inline std::string &ltrim(std::string &s);
+static inline std::string &rtrim(std::string &s);
+static inline std::string &trim(std::string &s);
 
 /* constants */
 const std::string version = "0.1";
@@ -28,7 +44,6 @@ unsigned int message_word_count = 0;
 unsigned int wrap = 40;
 std::string eyes = "oo";
 std::string tongue = "  ";
-std::string mouth = "  ";
 std::string cowpath = "/usr/share/cow";
 std::string cowfile = "default.cow";
 std::string message = "";
@@ -48,7 +63,6 @@ int main(int argc, const char *argv[])
                         cowpath << ":" << std::endl;
                 return EXIT_SUCCESS;
             case 'e':
-                //if (eyes.size() == 1) {
                 eyes.clear();
                 eyes += optarg[0];
                 eyes += optarg[1];
@@ -57,9 +71,9 @@ int main(int argc, const char *argv[])
                 cowfile = optarg;
                 break;
             case 'T':
-                mouth.clear();
-                mouth += optarg[0];
-                mouth += optarg[1];
+                tongue.clear();
+                tongue += optarg[0];
+                tongue += optarg[1];
                 break;
             case 'W':
                 try {
@@ -84,10 +98,10 @@ int main(int argc, const char *argv[])
 
 void display_usage(std::string exname, std::string e_version) {
     std::cout << "cxx{say,think} version " << e_version << 
-            ", (c) 2014 Duane Bekaert" << std::endl << "usage: " << 
+            ", (c) 2014 Duane Bekaert" << std::endl << "Usage: " << 
             exname << " [-bdgpstwy] [-h] [-e eyes] [-f cowfile] " << 
             std::endl << 
-            "          [-l] [-n] [-t tongue] [-w wrapcolumn] [message] " << 
+            "          [-l] [-n] [-T tongue] [-W wrapcolumn] [message] " << 
             std::endl;
 }
 
@@ -100,17 +114,24 @@ void display_cow() {
         std::cout << "-";
 
     if (message.size() < wrap) {
-        std::cout << std::endl << "< " << message << " >" << std::endl << " "; 
+        std::cout << std::endl << "< " << message << " >" << std::endl; 
     } else {
-
+        //auto substr_vec = break_down_message();
+        //std::cout << std::endl << "/" << std::endl;
+        //std::cout << "|" << std::endl;
+        //std::cout << "\\" << std::endl;
+        for(auto s : break_down_message()) {
+            std::cout << s << std::endl;
+        }
     }
 
+    std::cout << " ";
     for(int i = 0; i < message.size() + 2; i++)
         std::cout << "-";
     std::cout << std::endl;
 }
 
-void add_word_to_message(const std::string& word) {
+void add_word_to_message(const std::string & word) {
     if (word.empty()) return;
     
     if (message_word_count > 0)
@@ -124,6 +145,65 @@ void get_interactive_message() {
     std::istream_iterator<char> it(std::cin);
     std::istream_iterator<char> end;
     std::string results(it, end);
+
+    rep_map replace_list = {
+        {"\n", " "},
+        {"  ", " "} };
+
+    results = multi_replace(results, replace_list);
     
-    message = results;
+    //message = results;
+    message = trim(results);
+}
+
+std::vector<std::string> break_down_message() {
+    std::string str = message;
+    std::vector<std::string> ret;
+
+    unsigned int    counter = 0,
+                    prev_index = 0,
+                    index = 0; 
+
+    while (index < message.size()) {
+        index += wrap;
+        if (index > message.size()) {
+            ret.push_back(str.substr(prev_index));
+            break;
+        }
+        while(message[index]!=' ') {
+            index--;    
+        }
+        ret.push_back(str.substr(prev_index, index - prev_index));
+        prev_index = index;
+    }
+    return ret;
+}
+
+std::string multi_replace(std::string & str, rep_map e_rep_map) {
+    for ( it_rep_map it = e_rep_map.begin(); it != e_rep_map.end(); it++) {
+        for( size_t pos = 0; ; pos += it->second.length() ) {
+            pos = str.find( it->first, pos );
+            if( pos == std::string::npos ) 
+                break;
+            str.erase( pos, it->first.length() );
+            str.insert( pos, it->second);
+        }
+    }
+    return str;
+}
+
+static inline std::string &ltrim(std::string &s) {
+        s.erase(s.begin(), std::find_if(s.begin(),
+                s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
+        return s;
+}
+
+static inline std::string &rtrim(std::string &s) {
+        s.erase(std::find_if(s.rbegin(), s.rend(), 
+                std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
+        return s;
+}
+
+static inline std::string &trim(std::string &s) {
+        return ltrim(rtrim(s));
 }
